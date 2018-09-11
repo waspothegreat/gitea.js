91947bed020b007554de614dafdbda2af24eda08
Immo <seiryu@derg.immo>
2018-09-11 21:39:53 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# gitea.js
+
+This is a (WIP) library for Gitea using its REST API.
---
+++ index.js
@@ -0,0 +1,21 @@
+const request = require("node-superfetch");
+
+module.exports = class Gitea {
+    constructor(options = {}) {
+        this.options = options;
+        this._version = require("./package.json").version;
+        if (typeof this.options.url !== 'string') {
+            throw new TypeError('Inputted url is not a string.');
+        }
+    }
+    async version() {
+        return request.get(new URL("/api/v1/version", this.options.url)).then(ver => {
+            let version = {};
+            version.lib = this._version;
+            version.gitea = ver.body.version;
+            return version;
+        });
+        //return request.get(new (require('url')).URL('/api/v1/version', this.options.url)).then(ver => ({library: this._version, gitea: ver.body.version}));
+    }
+};
+
