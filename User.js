5a8865d5e81b15a7d9e8ca52afaf8cc3493a54c7
YourFurryGod <33117774+YourFurryGod@users.noreply.github.com>
2018-09-12 14:26:34 +0930

    fix multiple class req
    honestly idk what im doing but this fixes the token thing

--- User.js
+++ User.js
@@ -5,17 +5,16 @@ const Gitea = require('./Gitea.js');
  * @extends {Gitea}
  */
 module.exports = class User extends Gitea {
-    constructor(token) {
-        super();
-        this._options = super.options;
-        this.token = token;
-        if (!token) throw new ReferenceError('No authentication token inputted');
-        if (typeof token !== 'string') {
+    constructor(options) {
+        super(options);
+        this.token = options.token;
+        if (!this.token) throw new ReferenceError('No authentication token inputted');
+        if (typeof this.token !== 'string') {
             throw new ReferenceError('Inputted token is not a string');
         }
     }
     getUserInfo() {
-        return request.get(`${this._options.url}/api/v1/user?token=${this.token}`).then(r => r.body).catch(() => {
+        return request.get(`${this.options.url}/api/v1/user?token=${this.token}`).then(r => r.body).catch(() => {
             throw new TypeError('Authentication failure, please provide a valid token');
         });
     }
