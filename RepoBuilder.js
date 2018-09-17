569b322f90419a9691a0c2373a1c8439ac179d8c
waspothegreat <waspothegreat@gmail.com>
2018-09-17 16:24:20 -0400

    even more jsdoc

--- RepoBuilder.js
+++ RepoBuilder.js
@@ -2,26 +2,48 @@ module.exports = class RepoBuilder {
     constructor() {
         this.config = {auto_init: false, description: "", gitignores: "", license: "", name: "", private: false, readme: ""};
     }
+
+    /**
+    * @returns {Booleen}
+    */
     autoInit() {
         this.config.auto_init = true;
         return this;
     }
+
+    /**
+    * @param {string} desc
+    */
     setDescription(desc) {
         this.config.description = desc;
         return this;
     }
+    /**
+    * @param {string} gitignores
+    */
     setGitIgnores(gitignores) {
         this.config.gitignores = gitignores;
         return this;
     }
+    /**
+    * @param {string} license
+    */
     setLicense(license) {
         this.config.license = license;
         return this;
     }
+
+    /**
+    * @param {string} name
+    */
     setName(name) {
         this.config.name = name;
         return this;
     }
+
+    /**
+    * @returns {Booleen}
+    */
     private() {
         this.config.private = true;
         return this;
